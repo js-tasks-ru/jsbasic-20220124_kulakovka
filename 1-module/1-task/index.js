f4c789b6d377ae412c361eb89c4c435fb02f9886
kulakovka <kulakovka@gmail.com>
2022-02-01 19:53:23 +0300

    решение задачи 1.1 факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  let f = n;
+  let c = n;
+
+  if(n <= 1) {
+    return 1;
+  } else {
+
+    for(let i = 1; i < c; i++) {
+      f *= (n - 1);
+      n--;
+    }
+    return f;
+  }
 }
+factorial(5);
